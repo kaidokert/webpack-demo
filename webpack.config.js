a92f1ed7c6e228f58616e05d401e158d4ec54fe0
Tom Bridgwater <tom.bridgwater@zattoo.com>
2024-03-05 14:26:05 +0100

    Development

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {WebpackManifestPlugin} = require('webpack-manifest-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js',
@@ -11,7 +12,7 @@ module.exports = {
         new WebpackManifestPlugin({
         }),
         new HtmlWebpackPlugin({
-            title: 'Output Management',
+            title: 'Development',
         }),
     ],
     output: {
